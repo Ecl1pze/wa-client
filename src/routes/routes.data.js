9364f61a2d7b047b1ce6c27be4148609477a9054
Uwexur <uwexur@yandex.ru>
2024-10-12 11:06:18 +0300

    Added routes

---
+++ src/routes/routes.data.js
@@ -0,0 +1,31 @@
+import Auth from '../components/screens/auth/Auth'
+import Home from '../components/screens/home/Home'
+import NewWorkout from '../components/screens/new-workout/NewWorkout'
+import Profile from '../components/screens/profile/Profile'
+
+export const routes = [
+	{
+		path: '/',
+		exact: true,
+		component: Home,
+		isAuth: false
+	},
+	{
+		path: '/auth',
+		exact: false,
+		component: Auth,
+		isAuth: false
+	},
+	{
+		path: '/new-workout',
+		exact: false,
+		component: NewWorkout,
+		isAuth: false
+	},
+	{
+		path: '/profile',
+		exact: false,
+		component: Profile,
+		isAuth: false
+	}
+]
