7a925503e36aabba2402f1978de4c09e1796f5f7
Uwexur <uwexur@yandex.ru>
2024-10-12 11:17:38 +0300

    Added screens

---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom'
+import { Layout } from '../../layout/Layout'
+import Button from '../../ui/button/Button'
+import styles from './Home.module.scss'
+
+function App() {
+	const navigate = useNavigate()
+
+	return (
+		<>
+			<Layout bgImage='/images/home-bg.jpg'>
+				<Button clickHandler={() => navigate('/new-workout')}>New</Button>
+				<h1 className={styles.heading}>EXERCISES FOR THE SHOULDERS</h1>
+			</Layout>
+		</>
+	)
+}
+
+export default App
---
+++ src/components/screens/not-found/NotFound.jsx
@@ -0,0 +1,10 @@
+import { Layout } from '../../layout/Layout'
+
+const NotFound = () => {
+	return (
+		<Layout heading='Page not found'>
+			<div className='wrapper-inner-page'>404 page not found</div>
+		</Layout>
+	)
+}
+export default NotFound
